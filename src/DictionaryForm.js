ae09445a80ddc8462bbba75fa461ade6df3e249c
alimacy2508 <70335280+alimacy2508@users.noreply.github.com>
2024-10-17 14:05:40 +0100

    API integration
    Form now retrieves definitions corresponding to the searched word in the console

--- src/DictionaryForm.js
+++ src/DictionaryForm.js
@@ -1,11 +1,20 @@
 import React, { useState } from "react";
 import "./DictionaryForm.css";
+import axios from "axios";
+
 export default function DictionaryForm() {
   let [searchedword, setSearchedword] = useState("");
+
+  function handleResponse(response) {
+    console.log(response.data);
+  }
 
   function handleSubmit(event) {
     event.preventDefault();
     alert(`Searching for ${searchedword} defintion`);
+    let apiKey = "bbco0f50t952db4519a3613cfbc4bfb3";
+    let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${searchedword}&key=${apiKey}`;
+    axios.get(apiUrl).then(handleResponse);
   }
 
   function updateSearchedword(event) {
