e1e13ed5a8c35027fa9468b43a5349cf2809e3b1
alimacy2508 <70335280+alimacy2508@users.noreply.github.com>
2024-10-18 14:02:29 +0100

    Added a component to handle examples
    It will only display the example and heading now if there exists a example for the specific definition.

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Synonyms from "./Synonyms.js";
+import Example from "./Example.js";
 
 export default function Meaning(props) {
   console.log(props.meaning);
@@ -9,8 +10,9 @@ export default function Meaning(props) {
       <strong>Definition: </strong>
       <p>{props.meaning.definition}</p>
 
-      <strong>Example: </strong>
-      <em>{props.meaning.example}</em>
+      <em>
+        <Example example={props.meaning.example} />
+      </em>
       <Synonyms synonyms={props.meaning.synonyms} />
     </div>
   );
